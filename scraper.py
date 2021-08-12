bd46cea2be3dc06b480371f5a4a759463c8c5e6a
Avi Keinan <avik@doit-intl.com>
2021-08-13 01:12:58 +0300

    adding northamerica-northeast2 (toronto)

--- scraper.py
+++ scraper.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     a2_sud_discount = 1
     regions = ['us', 'us-central1', 'us-east1', 'us-east4', 'us-west4', 'us-west1', 'us-west2', 'us-west3', 'europe',
                'europe-central2', 'europe-west1', 'europe-west2', 'europe-west3', 'europe-west4', 'europe-west6', 'europe-north1',
-               'northamerica-northeast1', 'asia', 'asia-east', 'asia-east1', 'asia-east2', 'asia-northeast',
+               'northamerica-northeast1', 'northamerica-northeast2', 'asia', 'asia-east', 'asia-east1', 'asia-east2', 'asia-northeast',
                'asia-northeast1', 'asia-northeast2', 'asia-northeast3', 'asia-southeast', 'asia-southeast1',
                'australia-southeast1', 'australia-southeast2', 'australia', 'southamerica-east1', 'asia-south1', 'asia-southeast2', 'asia-south2']
 
