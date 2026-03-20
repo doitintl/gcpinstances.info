131de57bd81c5beeba227c93fc5922a010b2f02b
Avi Keinan <37156629+Burekasim@users.noreply.github.com>
2026-03-20 12:37:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: process.env.BASE_PATH || '/',
+})
+
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
